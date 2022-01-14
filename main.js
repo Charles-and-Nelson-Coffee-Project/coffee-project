1dcd90a2b4a382d145327ce74fe400a34308cec6
Charles Willcockson <willcockson.charles@gmail.com>
2022-01-14 16:58:03 -0600

    added extra coffees to the array

--- main.js
+++ main.js
@@ -6,18 +6,25 @@ let coffees = function () {
         return [
             {id: 1, name: 'Light City', roast: 'light', image: "/light-roast-coffee-bean.jpeg"},
             {id: 2, name: 'Half City', roast: 'light', image: "light-roast-coffee-bean.jpeg"},
-            {id: 3, name: 'Cinnamon', roast: 'light', image: "light-roast-coffee-bean.jpeg"},
-            {id: 4, name: 'City', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
-            {id: 5, name: 'American', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
-            {id: 6, name: 'Breakfast', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
-            {id: 7, name: 'High', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 8, name: 'Continental', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 9, name: 'New Orleans', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 10, name: 'European', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 11, name: 'Espresso', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 12, name: 'Viennese', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 13, name: 'Italian', roast: 'dark', image: "dark-roast.jpeg"},
-            {id: 14, name: 'French', roast: 'dark', image: "dark-roast.jpeg"}
+            {id: 3, name: 'Folgers', roast: 'light', image: "light-roast-coffee-bean.jpeg"},
+            {id: 4, name: 'Double Light', roast: 'light', image: "light-roast-coffee-bean.jpeg"},
+            {id: 5, name: 'Light Bulb', roast: 'light', image: "light-roast-coffee-bean.jpeg"},
+            {id: 6, name: 'Cinnamon', roast: 'light', image: "light-roast-coffee-bean.jpeg"},
+            {id: 7, name: 'City', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 8, name: 'American', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 9, name: 'Breakfast', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 10, name: 'Medrano', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 11, name: 'Spicy', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 12, name: 'Pickle Coffee', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 13, name: 'San Antonio', roast: 'medium', image: "medium-roasts-coffee-bean.jpeg"},
+            {id: 14, name: 'High', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 15, name: 'Continental', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 16, name: 'New Orleans', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 17, name: 'European', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 18, name: 'Espresso', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 19, name: 'Viennese', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 20, name: 'Italian', roast: 'dark', image: "dark-roast.jpeg"},
+            {id: 21, name: 'French', roast: 'dark', image: "dark-roast.jpeg"}
         ]
     }
 }();
